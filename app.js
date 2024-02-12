d6a69fe29508cbc3fb48c6e7b2711bdd638056b2
usich.ia <usich.ia@gmail.com>
2024-02-13 01:23:10 +0300

    asd

--- app.js
+++ app.js
@@ -2,3 +2,13 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 document.getElementById("customerName").innerHTML = tg.initDataUnsafe.user.first_name;
+
+let send = document.getElementById("send");
+
+send.addEVentListener("click", () {
+	tg.sendData('asdasdasdasdasdasdsda');
+	tg.close();
+});
+
+
+
