afea1891e07e059ca30e5f1fa21364740174af7d
usich.ia <usich.ia@gmail.com>
2024-02-13 15:41:09 +0300

    asbv

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ document.getElementById("customerName").innerHTML = tg.initDataUnsafe.user.first
 
 let send = document.getElementById("send");
 
-tg.sendData('asdasdasdasdasdasdsda');
+tg.sendData("asdasdasdasdasdasdsda");
 tg.close();
 
 // send.addEventListener("click", () => {
