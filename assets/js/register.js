b1d3f525d18e90902ee2ec07d5757c323fba7f35
usich.ia <usich.ia@gmail.com>
2024-02-14 02:04:47 +0300

    asdffds

--- assets/js/register.js
+++ assets/js/register.js
@@ -14,7 +14,7 @@ let data = {
 
 params = tg.initDataUnsafe.start_param;
 
-tg.showAlert(params);
+tg.showAlert('params');
 
 btnRegister = document.getElementById('btn-register');
 btnRegister.addEventListener('click',function(){
