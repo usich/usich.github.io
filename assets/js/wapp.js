3fb39316e43244a336fb9c7e1d74e7cadae0d753
usich.ia <usich.ia@gmail.com>
2024-02-14 02:35:44 +0300

    asdasda

--- assets/js/wapp.js
+++ assets/js/wapp.js
@@ -9,7 +9,7 @@ JsBarcode('#barcode', '9781022540897', {
 
 tg = window.Telegram.WebApp;
 	
-params = tg.initDataUnsafe;
+params = tg.initData;
 
 
 // params = tg.initDataUnsafe;
@@ -24,3 +24,7 @@ document.getElementById("profile-name").innerHTML = JSON.stringify(params);
 // Parameter	Type	Required	Description
 // web_app_query_id	String	Yes	Unique identifier for the query to be answered
 // result
+
+
+{"query_id":"AAFOPZgpAAAAAE49mCmt8kL3","user":{"id":697843022,"first_name":"Илья","last_name":"","username":"ilya_usichenko","language_code":"ru","allows_write_to_pm":true},"auth_date":"1707867139","hash":"13ebfd0b4b39a072f8db8f0a5823977aedbae62d69169aa4de4bd76404cf3003"}
+
