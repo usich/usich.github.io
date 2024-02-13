cc6f3bd92bee46b0fb31859a0db80357eee639f4
usich.ia <usich.ia@gmail.com>
2024-02-14 01:46:56 +0300

    sdfg

--- assets/js/wapp.js
+++ assets/js/wapp.js
@@ -7,9 +7,9 @@ JsBarcode('#barcode', '9781022540897', {
 
 tg = window.Telegram.WebApp;
 	
-params = tg.initDataUnsafe.start_param;
+// params = tg.initDataUnsafe.start_param;
 
-tg.showAlert(JSON.stringify(params));
+tg.showAlert("JSON.stringify(params)");
 // Use this method to set the result of an interaction with a Web App and send a corresponding message on behalf of the user to the chat from which the query originated. On success, a SentWebAppMessage object is returned.
 
 // Parameter	Type	Required	Description
