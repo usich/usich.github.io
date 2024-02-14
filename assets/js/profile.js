f9c837a593b8b2c1a80d41769b66c3ed5de409f3
usich.ia <usich.ia@gmail.com>
2024-02-14 17:38:32 +0300

    sdfsdf

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -17,31 +17,31 @@ function main(){
             let pair = queryParams[i].split("=");
             params[pair[0]] = pair[1];
         }
-        alert(JSON.stringify(initDataUnsafe));
-        alert(params['user-id']);
         if (params.hasOwnProperty('user-id')){
             userId = initDataUnsafe.user.id;
             userId = userId.toString();
-            alert(`${userId}: ${userId.length}. #### ${params['user-id']}: ${params['user-id'].length}`);
+            // alert(`${userId}: ${userId.length}. #### ${params['user-id']}: ${params['user-id'].length}`);
             if (params['user-id'] != userId) {
-
                 tg.close()
                 return;
             }
         } else {
-            alert("123");
             tg.close();
             return;
         }
         
+        if (!params.hasOwnProperty('barcode')){
+            tg.close();
+            return;
+        }
 
-        // JsBarcode('#barcode', '1231231231234', {
-        //     lineColor: "#0aa",
-        //     width:4,
-        //     height:60,
-        //     // height: 30,
-        //     displayValue: false
-        //     }); 
+        JsBarcode('#barcode', '1231231231234', {
+            lineColor: "#0aa",
+            width:4,
+            height:60,
+            // height: 30,
+            displayValue: false
+            }); 
     }
 }
 
