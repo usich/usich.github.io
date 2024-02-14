2bdbcf1be6ac3d82db8c86752b634626279acf48
usich.ia <usich.ia@gmail.com>
2024-02-15 00:30:02 +0300

    asd

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,25 +1,25 @@
 let tg = window.Telegram.WebApp;
-
-let firstName = document.getElementById('firstName');
-let lastName = document.getElementById('lastName');
-let middleName = document.getElementById('middleName');
-
 tg.expand();
-
-
-alert(typeof(firstName))
-let data = {
-	'asd': '1211212',
-	'firstName': firstName[0].value,
-	'lastName': lastName[0].value,
-	'middleName': middleName[0].value
-};
 
 // alert(firstName)
 btnRegister = document.getElementById('btn-register');
 btnRegister.addEventListener('click',function(){
-	tg.sendData(JSON.stringify(data));
-	tg.close();
-});
+	let firstName = document.getElementById('firstName');
+	let lastName = document.getElementById('lastName');
+	let middleName = document.getElementById('middleName');
+
+
+
+	
+	let data = {
+		'asd': '1211212',
+		'firstName': firstName.value,
+		'lastName': lastName.value,
+		'middleName': middleName.value
+	};
+
+		tg.sendData(JSON.stringify(data));
+		tg.close();
+	});
 
 
