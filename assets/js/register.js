5f196e205fab7c79341c9e3d6402963f92633ba3
usich.ia <usich.ia@gmail.com>
2024-02-14 23:20:05 +0300

    asdasd

--- assets/js/register.js
+++ assets/js/register.js
@@ -7,12 +7,13 @@ let middleName = document.getElementById('middleName');
 tg.expand();
 
 let data = {
+	'asd': '1211212',
 	'firstName': firstName.value,
 	'lastName': lastName.value,
 	'middleName': middleName.value
 };
 
-alert(firstName)
+// alert(firstName)
 btnRegister = document.getElementById('btn-register');
 btnRegister.addEventListener('click',function(){
 	tg.sendData(JSON.stringify(data));
