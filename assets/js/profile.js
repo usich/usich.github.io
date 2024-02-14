8a425b7c5cf0089b466a3a01895fe8263e6e33a8
usich.ia <usich.ia@gmail.com>
2024-02-14 16:27:18 +0300

    sdfsfdsf

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,11 +1,12 @@
 tg = window.Telegram.webApp;
-if (tg.initData == ''){
-   JsBarcode('#barcode', '1231231231234', {
-    lineColor: "#0aa",
-    width:4,
-    height:60,
-    // height: 30,
-    displayValue: false
-    }); 
-};
+if (tg.initData() == ''){
+    alert( "Правильно!" );
+    JsBarcode('#barcode', '1231231231234', {
+        lineColor: "#0aa",
+        width:4,
+        height:60,
+        // height: 30,
+        displayValue: false
+        }); 
+}
 
