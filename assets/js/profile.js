6c9ecfbf9970ca90b84c0e1305484c84f718a961
usich.ia <usich.ia@gmail.com>
2024-02-14 17:30:29 +0300

    dfsf

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -2,39 +2,47 @@ let tg = window.Telegram.WebApp;
 let initData = tg.initData || '';
 let initDataUnsafe = tg.initDataUnsafe || {};
 
-if (initData != '' && initDataUnsafe != {}){
+function main(){
+    if (initData != '' && initDataUnsafe != {}){
 
-    queryString = window.location.search;
-    queryString = queryString.substring(1);
-    if (queryString.includes('&')){
-        queryParams = queryString.split("&");
-    } else {
-           queryParams = queryString;
-       }   
-    params = {};
-    for (let i = 0; i < queryParams.length; i++) {
-        let pair = queryParams[i].split("=");
-        params[pair[0]] = pair[1];
-    }
-    alert(JSON.stringify(initDataUnsafe));
-    alert(params['user-id']);
-    if (params.hasOwnProperty('user-id')){
-        userId = initDataUnsafe.user.id;
-        userId = userId.toString();
-        alert(userId);
-        if (params['user-id'] != userId) {tg.close()};
-    } else {
-        alert("123");
-        tg.close();
-    }
-    
+        queryString = window.location.search;
+        queryString = queryString.substring(1);
+        if (queryString.includes('&')){
+            queryParams = queryString.split("&");
+        } else {
+               queryParams = queryString;
+           }   
+        params = {};
+        for (let i = 0; i < queryParams.length; i++) {
+            let pair = queryParams[i].split("=");
+            params[pair[0]] = pair[1];
+        }
+        alert(JSON.stringify(initDataUnsafe));
+        alert(params['user-id']);
+        if (params.hasOwnProperty('user-id')){
+            userId = initDataUnsafe.user.id;
+            userId = userId.toString();
+            alert(userId);
+            if (params['user-id'] != userId) {
+                tg.close()
+                return;
+            };
+        } else {
+            alert("123");
+            tg.close();
+            return;
+        }
+        
 
-    // JsBarcode('#barcode', '1231231231234', {
-    //     lineColor: "#0aa",
-    //     width:4,
-    //     height:60,
-    //     // height: 30,
-    //     displayValue: false
-    //     }); 
+        // JsBarcode('#barcode', '1231231231234', {
+        //     lineColor: "#0aa",
+        //     width:4,
+        //     height:60,
+        //     // height: 30,
+        //     displayValue: false
+        //     }); 
+    }
 }
+
+main();
 
