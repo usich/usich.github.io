47525ed43f2589c63bcf74d8c51190e7e69775f2
usich.ia <usich.ia@gmail.com>
2024-02-14 16:21:05 +0300

    asdasd

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,7 +1,11 @@
-JsBarcode('#barcode', '1231231231234', {
+tg = window.Telegram.webApp;
+if (tg.initData == ''){
+   JsBarcode('#barcode', '1231231231234', {
     lineColor: "#0aa",
     width:4,
     height:60,
     // height: 30,
     displayValue: false
-});
+    }); 
+};
+
