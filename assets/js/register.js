fc4116deb1c38b99f07eba33fffdf8d719f02fcb
usich.ia <usich.ia@gmail.com>
2024-02-15 13:06:54 +0300

    asdasd

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,25 +1,32 @@
 let tg = window.Telegram.WebApp;
-
+let initData = tg.initData || '';
+let initDataUnsafe = tg.initDataUnsafe || {};
 
 tg.expand();
 
+function main(){
 
-btnRegister = document.getElementById('btn-register');
-btnRegister.addEventListener('click',function(){
-	let firstName = document.getElementById('firstName');
-	let lastName = document.getElementById('lastName');
-	let middleName = document.getElementById('middleName');
-	let email = document.getElementById('email');
+	alert(JSON.stringify(initData));
+	alert(JSON.stringify(initDataUnsafe));
+	tg.close();
+	return;
 
-	let data = {
-		'firstName': firstName.value,
-		'lastName': lastName.value,
-		'middleName': middleName.value,
-		'email': email.value
-	};
+	btnRegister = document.getElementById('btn-register');
+	btnRegister.addEventListener('click',function(){
+		let firstName = document.getElementById('firstName');
+		let lastName = document.getElementById('lastName');
+		let middleName = document.getElementById('middleName');
+		let email = document.getElementById('email');
 
-		tg.sendData(JSON.stringify(data));
-		tg.close();
-	});
+		let data = {
+			'firstName': firstName.value,
+			'lastName': lastName.value,
+			'middleName': middleName.value,
+			'email': email.value
+		};
 
+			tg.sendData(JSON.stringify(data));
+			tg.close();
+		});
+}
 
