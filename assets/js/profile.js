8cff257ca45e2de029f02995641a62cb1caa6874
usich.ia <usich.ia@gmail.com>
2024-04-11 18:51:17 +0300

    adasd

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -6,7 +6,7 @@ let data  = {
     'init_data': initData
 };
 
-fetch('https://6d1f-5-228-34-19.ngrok-free.app/api/v1/auth/login', {
+fetch('https://7a83-109-173-72-27.ngrok-free.app/api/v1/auth/login', {
   method: 'POST', // Здесь так же могут быть GET, PUT, DELETE
   body: JSON.stringify(data), // Тело запроса в JSON-формате
   headers: {
